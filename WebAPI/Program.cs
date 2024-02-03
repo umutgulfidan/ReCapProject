bb2425ce32a5a9e42f1c7ce83e2ac6eb0acf2b33
umutgulfidan <umutgulfidan@gmail.com>
2024-02-03 10:25:21 +0300

    Kodlama.io Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR) 12.Gün 1.Ödev -- Autofac

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,16 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 
 // Add services to the container.
 
@@ -12,23 +19,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //IoC
-builder.Services.AddSingleton<ICarService,CarManager>();
-builder.Services.AddSingleton<ICarDal, EfCarDal>();
+//builder.Services.AddSingleton<ICarService,CarManager>();
+//builder.Services.AddSingleton<ICarDal, EfCarDal>();
 
-builder.Services.AddSingleton<IBrandService,BrandManager>();
-builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
+//builder.Services.AddSingleton<IBrandService,BrandManager>();
+//builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
 
-builder.Services.AddSingleton<IColorService,ColorManager>();
-builder.Services.AddSingleton<IColorDal,EfColorDal>();
+//builder.Services.AddSingleton<IColorService,ColorManager>();
+//builder.Services.AddSingleton<IColorDal,EfColorDal>();
 
-builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+//builder.Services.AddSingleton<ICustomerService, CustomerManager>();
+//builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
 
-builder.Services.AddSingleton<IUserService, UserManager>();
-builder.Services.AddSingleton<IUserDal, EfUserDal>();
+//builder.Services.AddSingleton<IUserService, UserManager>();
+//builder.Services.AddSingleton<IUserDal, EfUserDal>();
 
-builder.Services.AddSingleton<IRentalService, RentalManager>();
-builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+//builder.Services.AddSingleton<IRentalService, RentalManager>();
+//builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
 
 
 var app = builder.Build();
